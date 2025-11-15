b495b413f8b6a134ea19a0373d1da95bac205f3b
MaaikePh <maaikephielix@live.nl>
2025-11-15 20:23:43 +0100

    Aangepast: AuthContext.jsx met useEffect en een verbeterde login functie

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,5 +1,7 @@
-import {createContext, useState} from 'react';
+import {createContext, useEffect, useState} from 'react';
 import {useNavigate} from 'react-router-dom';
+import {jwtDecode} from 'jwt-decode';
+import axios from 'axios';
 
 export const AuthContext = createContext({});
 
@@ -7,26 +9,90 @@ function AuthProvider({children}) {
     const [isAuthenticated, setIsAuthenticated] = useState({
         isAuth: false,
         user: null,
+        status: 'pending',
     });
+
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+
+        async function fetchUser() {
+            if (!token) {
+                setIsAuthenticated({
+                    isAuth: false,
+                    user: null,
+                    status: 'done'
+            });
+                return;
+        }
+
+        try {
+            const decodedToken = jwtDecode(token);
+            const userId = decodedToken.id;
+
+            const response = await axios.get(`https://novi-backend-api-wgsgz.ondigitalocean.app/api/users/${userId}`, {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                        'novi-education-project-id': '07470393-2b91-4dbf-92b8-976e6532490b',
+                    },
+                }
+            )
+
+            setIsAuthenticated({
+                isAuth: true,
+                user: {
+                    id: response.data.id,
+                    username: response.data.username,
+                    email: response.data.email,
+                    roles: response.data.roles,
+                },
+                status: 'done',
+            });
+        } catch (error) {
+            console.error(error);
+            localStorage.removeItem('token');
+
+            setIsAuthenticated({
+                isAuth: false,
+                user: null,
+                status: 'done',
+            })
+          }
+        }
+
+        fetchUser();
+    }, []);
+
     const navigate = useNavigate();
 
     function login(userDetails) {
         console.log(userDetails);
+
         localStorage.setItem('token', userDetails.token);
+
         setIsAuthenticated({
             isAuth: true,
             user: {
+                username: userDetails.username,
                 email: userDetails.email,
                 roles: userDetails.roles,
-            }
+            },
+            status: 'done',
             });
+
         console.log('Gebruiker is ingelogd!');
+
         navigate('/profile');
     }
 
     function logout() {
-        setIsAuthenticated(false);
+        setIsAuthenticated({
+            isAuth: false,
+            user: null,
+            status: 'done',
+        });
+
         console.log('Gebruiker is uitgelogd!');
+
         navigate('/');
     }
 
@@ -39,7 +105,7 @@ function AuthProvider({children}) {
 
     return (
         <AuthContext.Provider value={data}>
-            {children}
+            {isAuthenticated.status === 'done' ? children : <p>Loading...</p>}
         </AuthContext.Provider>
     )
 }
